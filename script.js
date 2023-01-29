3eb1e161066a2d5538dc27906019bdab67206af0
GiuliaGlave <giulia.glave@gmail.com>
2023-01-29 15:32:28 +0100

    add variabili e condizioni

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 const obtain = document.getElementById('btn-obtain');
+
+//DICHIARAZIONE VARIABILI INPUT
+const userName = document.getElementById("name").value;
+const userKmEl = document.getElementById('km');
+const userAgeEl = document.getElementById('age');
+
+//DICHIARAZIONE VARIABILI OUTPUT
+const namePassenger = document.getElementById("name")
+const ticketTypeEl = document.getElementById("ticket-type")
+const trainCarriageEl = document.getElementById("train-carriage")
+const codeCPEl = document.getElementById("cp-code")
+const ticketFinalPrice = document.getElementById("total")
+
 obtain.addEventListener(
 
     "click",
     function () {
 
         /* DICHIARAZIONE VARIABILI */
-        const userName = document.getElementById("name").value;
-        const km = parseInt(document.getElementById('km').value);
-        const age = parseInt(document.getElementById('age').value);
+        const userKm = userKmEl.value
+        const userAge = userAgeEl.value
 
-        const standardPrice = km * 0.21;
-        let price;
+        const pricePerKm = 0.21;
+        const standardPrice = userKm * pricePerKm;
 
-        if (!isNaN(km)) {
+        if (userAge == "minorenne") {
 
-            if (age == "Minorenne") {
+            const discountMinor = 20 / 100;
+            const discountTotalMinor = standardPrice * discountMinor;
+            price = standardPrice - discountTotalMinor;
 
-                price = (standardPrice - (standardPrice * .2));
+        } else if (userAge == "over-65") {
+            const discountOver65 = 40 / 100;
+            const discountTotalOver65 = standardPrice * discountOver65;
+            price = standardPrice - discountTotalOver65;
 
-            } else if (age == "Over 65") {
+        } else {
 
-                price = (standardPrice - (standardPrice * .4));
+            price = standardPrice;
 
-            } else {
-
-                price = standardPrice;
-
-            }
-        }
-
-        else {
-            alert('inserire un valore numerico per i km')
         }
 
         /* DECIMALI */
         price = price.toFixed(2);
-        console.log(price);
+
+        //CODICI E DATI BIGLIETTO
+        let trainCarriage = Math.floor(Math.random() * 10) + 1;
+        let codeCP = Math.floor(Math.random() * 100000) + 1;
+
+        codeCPEl.innerHTML = codeCP;
+        trainCarriageEl.innerHTML = trainCarriage;
 
         /* TOTALE */
         document.getElementById('total').innerHTML = `TOTALE BIGLIETTO: ${price} €`;
     }
 )
-
