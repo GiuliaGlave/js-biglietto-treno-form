2272f7642951a96ed06d1bd9dfc1f14f189854a2
GiuliaGlave <giulia.glave@gmail.com>
2023-01-30 12:09:33 +0100

    validator check

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const obtain = document.getElementById('btn-obtain');
 
 //DICHIARAZIONE VARIABILI INPUT
-const userName = document.getElementById("name").value;
+let userNameEl = document.getElementById("name").value;
+let userName = userNameEl;
+
 const userKmEl = document.getElementById('km');
 const userAgeEl = document.getElementById('age');
 
 //DICHIARAZIONE VARIABILI OUTPUT
-const namePassenger = document.getElementById("name")
-const ticketTypeEl = document.getElementById("ticket-type")
-const trainCarriageEl = document.getElementById("train-carriage")
-const codeCPEl = document.getElementById("cp-code")
-const ticketFinalPrice = document.getElementById("total")
+const namePassengerEl = document.getElementById("name-on-ticket").value;
+let namePassenger = namePassengerEl;
+
+const ticketTypeEl = document.getElementById("ticket-type");
+let ticketType = ticketTypeEl;
+
+const trainCarriageEl = document.getElementById("train-carriage");
+const codeCPEl = document.getElementById("cp-code");
+const ticketFinalPriceEl = document.getElementById("total");
 
 obtain.addEventListener(
 
@@ -23,35 +29,40 @@ obtain.addEventListener(
 
         const pricePerKm = 0.21;
         const standardPrice = userKm * pricePerKm;
+        let finalPrice=standardPrice
 
         if (userAge == "minorenne") {
 
             const discountMinor = 20 / 100;
             const discountTotalMinor = standardPrice * discountMinor;
-            price = standardPrice - discountTotalMinor;
+            finalPrice = standardPrice - discountTotalMinor;
 
         } else if (userAge == "over-65") {
             const discountOver65 = 40 / 100;
             const discountTotalOver65 = standardPrice * discountOver65;
-            price = standardPrice - discountTotalOver65;
+            finalPrice = standardPrice - discountTotalOver65;
 
         } else {
 
-            price = standardPrice;
-
+            finalPrice = standardPrice;
+            
         }
 
         /* DECIMALI */
-        price = price.toFixed(2);
+        finalPrice = finalPrice.toFixed(2);
 
         //CODICI E DATI BIGLIETTO
-        let trainCarriage = Math.floor(Math.random() * 10) + 1;
+
         let codeCP = Math.floor(Math.random() * 100000) + 1;
+        let trainCarriage = Math.floor(Math.random() * 10) + 1;
+        
 
         codeCPEl.innerHTML = codeCP;
         trainCarriageEl.innerHTML = trainCarriage;
 
+     
         /* TOTALE */
-        document.getElementById('total').innerHTML = `TOTALE BIGLIETTO: ${price} €`;
+        ticketFinalPriceEl.innerHTML = `TOTALE BIGLIETTO: ${finalPrice} €`;
     }
 )
+
